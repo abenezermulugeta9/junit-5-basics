12428951262074997c86cd1da70c1703de220032
AbenezerM <abenezermulugeta41@gmail.com>
2022-12-01 01:34:51 -0600

    Project setup and initial commit

---
+++ src/main/java/io/abenezermulugeta/MathUtils.java
@@ -0,0 +1,7 @@
+package io.abenezermulugeta;
+
+public class MathUtils {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/MathUtilsTest.java
@@ -0,0 +1,12 @@
+import io.abenezermulugeta.MathUtils;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class MathUtilsTest {
+
+    @Test
+    void test() {
+        System.out.println("This test is running");
+    }
+}
