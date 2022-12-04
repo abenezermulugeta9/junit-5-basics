5f2eb116b7f01a55d59027a59a3ffe20f1c01dd6
AbenezerM <abenezermulugeta41@gmail.com>
2022-12-04 04:02:04 -0600

    Testing exceptions and implement test lifecycle hooks

--- src/test/java/MathUtilsTest.java
+++ src/test/java/MathUtilsTest.java
@@ -1,12 +1,44 @@
 import io.abenezermulugeta.MathUtils;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MathUtilsTest {
+    MathUtils mathUtils;
+
+    // Lifecycle hooks
+    /*
+    * These two methods need to be static methods, because they have to be executed after the test class is constructed
+    * @BeforeAll
+    * @AfterAll
+    *
+     * @BeforeEach
+    * @AfterEach
+    * */
+    @BeforeEach
+    void init() {
+        this.mathUtils = new MathUtils();
+    }
+
+    @AfterAll
+    static void cleanUp() {
+        System.out.println("Cleaning up....");
+    }
 
     @Test
-    void test() {
-        System.out.println("This test is running");
+    void testAdd() {
+        int expectedResult = 3;
+        int actualResult = mathUtils.add(1, 2);
+        assertEquals(expectedResult, actualResult, "The add() method should return the addition of the two arguments.");
+    }
+
+    @Test
+    void testDivideException() {
+        assertThrows(ArithmeticException.class, () -> mathUtils.divide(2, 1), "Division by 0 should return ArithmeticException.");
+    }
+
+    @Test
+    void testComputeCircleArea() {
+        assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "The computeCircleArea() method should calculate and return the right area.");
     }
 }
